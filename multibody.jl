5ef5a427004f40b322b1d739bd5709a4acb924a5
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2025-10-05 14:31:01 +0200

    pair

--- multibody.jl
+++ multibody.jl
@@ -1,5 +1,11 @@
 using CSV, DataFrames
+using DynamicPolynomials
+using MomentOpt
+using MosekTools
+using LinearAlgebra
 using PGFPlots
+using Random
+Random.seed!(1)
 
 D = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
     (d -> d[:,["frame_id","x","y","vx","vy"]]) |>
@@ -11,7 +17,6 @@ D = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
 frames = unique(D[:,"frame_id"]) .|> Int
 counts = [sum(D[:,"frame_id"] .== f) for f in frames]
 mframe = frames[counts .> 1]
-save("agents.pdf", Plots.Histogram(counts))
 
 function sample()
     rf = rand(mframe)
@@ -19,19 +24,43 @@ function sample()
     ra = rd[randperm(size(rd,1))[1:2],:]
     return ra
 end
-D2 = [sample() for _ in 1:1000] .|>
+D2 = [sample() for _ in 1:100] .|>
     (d -> [d[1,"x"],d[1,"y"],d[2,"x"],d[2,"y"],d[1,"vx"],d[1,"vy"],d[2,"vx"],d[2,"vy"]]) |>
     stack
 
+d = 2
+@polyvar x[1:8]
+M2 = sum(DiracMeasure(x,collect(s)) for s in eachcol(D2)) / size(D2,2)
+Λ2 = let v = monomials(x,0:d);
+    Q2 = integrate.(v*v',[M2]);
+    v'*inv(Q2+1e-4I)*v
+end
+
+ϕ = monomials(x[1:4],0:2d)
+m = GMPModel(Mosek.Optimizer)
+@variable m ρ  Meas(x,support=@set(x'x<=10))
+@variable m ρT Meas(x,support=@set(x'x<=10))
+ρ0 = DiracMeasure(x,D2[:,1])
+@objective m Min Mom(Λ2,ρ)
+@constraint m Mom.(differentiate(ϕ,x[1:4])*x[5:8],ρ) - Mom.(ϕ,ρT) .== -integrate.(ϕ,ρ0)
+@constraint m Mom(1,ρ) == 1
+optimize!(m)
+
+q1 = let v = monomials(x[1:2],0:d);
+    Q = integrate.(v*v',[ρ]);
+    v'*inv(Q+1e-4I)*v
+end
+q2 = let v = monomials(x[3:4],0:d);
+    Q = integrate.(v*v',[ρ]);
+    v'*inv(Q+1e-4I)*v
+end
+
 save("multibody.pdf", Axis([
+    Plots.Image((x,y)->1/q1(x,y)+1/q2(x,y),(-1,1),(-1,1)),
     Plots.Quiver(
         D2[1,:],   D2[2,:],
         D2[5,:]/3, D2[6,:]/3,
         style="-stealth, no markers, blue"
     ),
-    Plots.Quiver(
-        D2[3,:],   D2[4,:],
-        D2[7,:]/3, D2[8,:]/3,
-        style="-stealth, no markers, red"
-    ),
-]))
+    Plots.Scatter(reshape(D2[1:4,1],(2,2)))
+],xmin=-1,xmax=1,ymin=-1,ymax=1))
