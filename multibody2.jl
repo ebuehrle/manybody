5d1553d043f2cdea147d6fb2edd3410b9fbe5dd6
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2025-11-13 12:18:22 +0100

    gt

--- multibody2.jl
+++ multibody2.jl
@@ -16,7 +16,7 @@ frames = D[:,"frame_id"] .|> Int |> unique
 counts = [sum(D[:,"frame_id"] .== f) for f in frames]
 weight = binomial.(counts, 2)
 
-d = 3
+d = 2
 @polyvar t x[1:8] x1[1:4] x2[1:4]
 M = sum(DiracMeasure(x1,s) for s in collect.(eachrow(D[:,["x","y","vx","vy"]]))) / length(frames)
 K0 = let v0 = monomials(x1,0:d);
@@ -55,6 +55,24 @@ q2 = [let v = monomials(x[3:4],0:d);
     v'*inv(Q+1e-4I)*v
 end for i=1:length(x0)]
 
+id0 = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
+    (d -> d[:,["track_id","frame_id","x","y","vx","vy"]]) |>
+    (d -> d .- [0 0 1000 1000 0 0]) |>
+    (d -> d ./ [1 1 20 20 20 20]) |>
+    (d -> filter(e -> -1 <= e["x"] <= 1, d)) |>
+    (d -> filter(e -> -1 <= e["y"] <= 1, d)) |>
+    (m -> filter(e -> e["frame_id"] == f0, m)) |>
+    (m -> m[:,"track_id"]) |> unique
+f1 = f0 + 3*10
+X1 = CSV.read("vehicle_tracks_000.csv", DataFrame) |> 
+    (m -> filter(e -> e["frame_id"] == f1, m)) |>
+    (m -> filter(e -> e["track_id"] ∈ id0, m)) |>
+    (m -> m[:,["x","y","vx","vy"]]) |>
+    (d -> d .- [1000 1000 0 0]) |>
+    (d -> d ./ [20 20 20 20]) |>
+    (d -> filter(e -> -1 <= e["x"] <= 1, d)) |>
+    (d -> filter(e -> -1 <= e["y"] <= 1, d))
+
 save("multibody2-$(fi).pdf", Axis([
     Plots.Image((x,y)->1/q1(x,y)+sum(1/q(x,y) for q in q2),(-1,1),(-1,1)),
     Plots.Quiver(
@@ -63,6 +81,7 @@ save("multibody2-$(fi).pdf", Axis([
         style="-stealth, no markers, blue"
     ),
     Plots.Scatter(X0),
-    Plots.Scatter([integrate.(x[1:2],[ρT[1]]) integrate.(x[3:4],transpose(ρT))],style="red"),
+    Plots.Scatter([integrate.(x[1:2],[ρT[1]]) integrate.(x[3:4],transpose(ρT))],mark="x",style="red"),
+    Plots.Scatter(X1,mark="x",style="green"),
 ],xmin=-1,xmax=1,ymin=-1,ymax=1))
 end
